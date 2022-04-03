57a68527323ab03d50f6d6856dcc43df1299be8f
Guleid0 <guleidac@gmail.com>
2022-04-03 13:56:57 +0300

    done

--- challenge.js
+++ challenge.js
@@ -1,2 +1,14 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+let redScore  = 96 + 108 + 89;
+let redAverage = redScore/3;
+let blueScore  = 88 + 91 + 110;
+let blueAverage = blueScore/3;
+if(redAverage > blueAverage){
+    console.log("the red won")
+} else if(redAverage < blueAverage){
+    console.log("the blue won")
+} else{
+    console.log("drow")
+}
