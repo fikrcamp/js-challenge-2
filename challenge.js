ab09bdac208573b23b3831b750d632871e8af051
hoodfk <umaliita240@gmail.com>
2022-04-03 11:18:05 +0300

    done with the challenge

--- challenge.js
+++ challenge.js
@@ -1,2 +1,17 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+let redTeams_score1 = 96 ;
+let redTeams_score2 = 108 ;
+let redTeams_score3 = 89 ;
+let redTeams_average = redTeams_score1 + redTeams_score2 + redTeams_score3 / 3;
+ console.log(redTeams_average);
+
+ let blueTeams_score1 = 88 ;
+let blueTeams_score2 = 91 ;
+let blueTeams_score3 = 110 ;
+let blueTeams_average = blueTeams_score1 + blueTeams_score2 + blueTeams_score3 / 3;
+ console.log(blueTeams_average);
+
+ console.log(redTeams_average > blueTeams_average);
+
+ console.log("that means the red team is the winner since they've got the higher average")
