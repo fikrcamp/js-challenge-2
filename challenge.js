54adb4b7c7b162363c38f226d8f4b2fe20baf97c
Suhayb Mohamed <suhaybmaxamed@gmail.com>
2022-04-04 09:19:06 +0300

    the challenge is done

--- challenge.js
+++ challenge.js
@@ -1,2 +1,27 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+// Red Team score
+const redFirst = 96;
+const redSecond = 108;
+const redThird = 89;
+
+// Red Team Average Score
+const redAverage = Math.floor(redFirst + redSecond + redThird / 3);
+console.log(redAverage);
+
+// Blue Team score
+const blueFirst = 88;
+const blueSecond = 91;
+const blueThird = 110;
+
+// Red Team Average Score
+const blueAverage = Math.floor(blueFirst + blueSecond + blueThird / 3);
+console.log(blueAverage);
+
+// Compare Team Scores
+if (redFirst > redSecond) {
+  console.log("Red Team is the winner");
+} else {
+  console.log("Blue Team is the winner");
+}
