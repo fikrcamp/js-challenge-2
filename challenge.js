efef3fc5c90d81a3e19c6b15e7d3d19c4587f07d
Mustafe <apple@Mustafe.local>
2022-04-04 12:07:53 +0300

    js-challenge-2

--- challenge.js
+++ challenge.js
@@ -1,2 +1,12 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+let averageReds =(96+108+89)/3;
+let averageBlues=(88+91+110)/3;
+console.log (averageReds);
+console.log (averageBlues);
+
+if (averageReds > averageBlues) {
+    console.log ("Red Team are Winners");
+} else { console.log ("Blue Team are winners");
+}
