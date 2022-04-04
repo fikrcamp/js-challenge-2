af09f436a1b4f2acb22b305f0c4d351b056a2e67
Yuhanaz <yoonis7881@gmail.com>
2022-04-04 10:22:20 +0300

    js -chalenge -1 is completed

--- challenge.js
+++ challenge.js
@@ -1,2 +1,25 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+// Red team scores
+let rscore1 = 96;
+let rscore2 = 108;
+let rscore3 = 89;
+let redAverage= (rscore1 + rscore2 + rscore3)/3
+// console.log(redAverage)
+
+// blue team scores
+let bscore1 = 88;
+let bscore2 = 91; 
+let bscore3 = 110;
+let blueAverage= (bscore1 + bscore2 + bscore3)/3
+// console.log(redAverage)
+
+if (redAverage > blueAverage){
+    console.log("Red team are the Winner");
+} else if (redAverage < blueAverage){
+    console.log("Blue team are the Winner")
+}
+else{
+    console.log("Draw")
+}
+
