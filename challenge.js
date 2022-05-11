78c2c3272cde3c844b0253a762db3dcdea4c0906
Apple <juriabdallah@gmail.com>
2022-05-11 16:50:02 +0300

    donex4

--- challenge.js
+++ challenge.js
@@ -1,2 +1,15 @@
-// Good Luck! You got this 💪🏾
-// Write your code here.
+// Data: Reds score 96, 108 and 89. Blues score 88, 91 and 110
+
+let redAvg= 96+108+89 /3;
+let blueAvg= 88+91+110 /3;
+console.log(
+ redAvg
+)
+
+if ( redAvg > blueAvg) { 
+    console.log (" Red team won");
+} else {
+    console.log (" Blue team won");
+} 
+
+
